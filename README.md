e95816f2864194c9c5ba6a69cd6c8b15dcb469eb
paul <paul@genesis.websealevel.com*>
2022-04-02 09:40:31 +0200

    README et init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # doc-phpxml
+
+Documentation et exemples sur la manipulation de XML en php
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$xml = simplexml_load_file('data.xml');
+
+//Ca marche pas
+// var_dump($xml['plugins']);
+
+
+var_dump($xml->children('theme_official_plugins'));
