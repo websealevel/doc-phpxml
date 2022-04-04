fc67e49777a86bd69d9d463c86c61ed56342aca3
paul <paul@genesis.websealevel.com*>
2022-04-04 12:20:35 +0200

    mise a jour

--- index.php
+++ index.php
@@ -4,28 +4,28 @@ $xml_file = 'data.xml';
 
 
 //Validation du docment XML avec un DTD
+
+//On cree une abstraction du DOM
 $dom = new DOMDocument();
+
+//On valide le XML avec le DTD
 $dom->load($xml_file);
 if ($dom->validate()) {
     echo "Le document XML " . $xml_file . " est valide." . PHP_EOL;
 }
+//On cree une abstraction de XPath a partir du dom
+$xpath = new DOMXpath($dom);
 
-$xml = simplexml_load_file($xml_file);
+//On enregistre un namespace pour ne pas avoir à le taper à chaque query
+$xpath->registerNamespace('ns', 'https://websealevel.com/wordpress/plugins/wsl_theme_dependencies'); 
 
-// var_dump($xml);
-
-//Acceder au premier element du document par indice
-// var_dump($xml->theme[0]->plugins);
-
-// foreach ($xml->children() as $theme) {
-//     var_dump($theme['name']);
-//     echo "<br>";
-// }
-
-//Selectionne les noeuds 'theme' enfant du noeud courant
-$plugins = $xml->xpath("theme_official_plugins/theme");
-var_dump($plugins);
+//Query
 
 //Selectionne le noeud 'theme' dont l'attribut name vaut atelierduboisdor, enfant du noeud courant
-$plugins = $xml->xpath('theme_official_plugins/theme[@name="atelierduboisdor"]');
-var_dump($plugins);
+$themes = $xpath->query('//ns:theme_official_plugins/ns:theme');
+
+//Exploite les résultats
+
+foreach($themes as $theme){
+    var_dump($theme);
+}
